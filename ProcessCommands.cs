e2aa7f4789c2900c398e43e2276b7e9903e240e9
Josh Gustafson <j@gustafson.me>
2016-07-26 10:25:02 -0600

    Pipe console output into the main log

--- ProcessCommands.cs
+++ ProcessCommands.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Threading;
 using System.Xml.Serialization;
 
 namespace MCEControl {
@@ -30,16 +31,75 @@ namespace MCEControl {
         }
 
         public override void Execute(Reply reply) {
-            MainWindow.AddLogEntry("Cmd: Starting process: " + File + " " + Arguments);
             if (File != null) {
-                var p = new Process {StartInfo = {FileName = File, Arguments = Arguments}};
-                p.Start();
+                var executor = new Executor(File, Arguments);
+                executor.Run();
+
                 if (NextCommand != null)
-                    p.WaitForInputIdle(10000); // TODO: Make this settable
+                    executor.WaitForInputIdle(TimeSpan.FromSeconds(10)); // TODO: Make this settable
             }
 
             if (NextCommand != null)
                 NextCommand.Execute(reply);
+        }
+
+        private class Executor
+        {
+            public Executor(string File, string Arguments)
+            {
+                process = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = File,
+                        Arguments = Arguments,
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                    },
+                };
+                process.OutputDataReceived += OnOutputReceived;
+                process.ErrorDataReceived += OnErrorReceived;
+                process.EnableRaisingEvents = true;
+            }
+
+            public void Run()
+            {
+                new Thread(() =>
+                {
+                    process.Start();
+                    Log($"Started process \"{process.StartInfo.FileName}\" with arguments \"{process.StartInfo.Arguments}\"");
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+                    process.WaitForExit();
+                    Log($"Process exited with code {process.ExitCode}");
+                }).Start();
+            }
+
+            public void WaitForInputIdle(TimeSpan timeSpan)
+            {
+                process.WaitForInputIdle((int)timeSpan.TotalMilliseconds);
+            }
+
+            private Process process;
+
+            private void Log(string message)
+            {
+                MainWindow.AddLogEntry($"[{process.Id:D5}] {message}");
+            }
+
+            private void OnOutputReceived(object sender, DataReceivedEventArgs e)
+            {
+                if (e == null || e.Data == null) { return; }
+                Log($"OUT: {e.Data}");
+            }
+
+            private void OnErrorReceived(object sender, DataReceivedEventArgs e)
+            {
+                if (e == null || e.Data == null) { return; }
+                Log($"ERR: {e.Data}");
+            }
         }
     }
 }
